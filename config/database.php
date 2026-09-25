49737c7028baa6036d0e4c5550f62125f1eb8d77
Priyanshu Yadav <priyanshuyadav-it@gmail.com>
2026-09-25 18:50:22 +0530

    2026-09-25: Added database connection

---
+++ config/database.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = "localhost";
+$dbname = "student_skill_portal";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password
+    );
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+
+?>
